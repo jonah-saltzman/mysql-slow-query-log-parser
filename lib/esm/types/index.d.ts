741eb9ad723793bfe14168cab87323b4b931f467
Jonah Saltzman <jonahsaltzman@gmail.com>
2023-01-12 20:03:49 -0500

    version 0.1

--- lib/esm/types/index.d.ts
+++ lib/esm/types/index.d.ts
@@ -1,5 +1,6 @@
 /// <reference types="node" />
 import * as fs from 'fs';
 import { LogFileEntry } from './classes';
+export { LogFileParser, LogFileEntry } from './classes';
 export declare function parseStream(readable: fs.ReadStream): Promise<LogFileEntry[]>;
 //# sourceMappingURL=index.d.ts.map
